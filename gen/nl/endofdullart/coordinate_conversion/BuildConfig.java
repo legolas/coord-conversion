9175094b6e979be619513c3723309f79f0e51449
legolas <legolasgreenlf@gmail.com>
2012-07-30 16:32:30 +0200

    Initial commit

---
+++ gen/nl/endofdullart/coordinate_conversion/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package nl.endofdullart.coordinate_conversion;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
