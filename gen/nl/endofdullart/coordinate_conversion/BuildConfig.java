970e68122f885640df65415ea85bbf80be900e82
Marcel Dullaart <legolasgreenlf@gmail.com>
2012-08-01 15:01:11 +0200

    Removed generated files

--- gen/nl/endofdullart/coordinate_conversion/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package nl.endofdullart.coordinate_conversion;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
