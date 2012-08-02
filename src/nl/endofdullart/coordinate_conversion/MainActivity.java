f064170f8e290137fcc0bdfc7f10ee3407637650
Marcel Dullaart <legolasgreenlf@gmail.com>
2012-08-02 15:42:51 +0200

    Organized imports

--- src/nl/endofdullart/coordinate_conversion/MainActivity.java
+++ src/nl/endofdullart/coordinate_conversion/MainActivity.java
@@ -1,23 +1,21 @@
 package nl.endofdullart.coordinate_conversion;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
 
-    
 }
